db138a58b8631cd56d5705db96d12e78bee30e19
donutwizard666 <thebeardedwarrior@icloud.com>
2019-10-14 12:39:14 -0700

    project set up. ran install command for yarn, imported browser router to index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route } from "react-router-dom";
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { from } from 'rxjs';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" component={Home} />
   </div>
+  
 );
 
 export default App;
